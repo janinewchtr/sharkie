ae5670375e9470ebca3d12e95d553b77c685dad2
Janine Wächter <156570900+janinewchtr@users.noreply.github.com>
2026-03-12 16:52:37 +0100

    changed energy lost to -20 per collision

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -47,7 +47,7 @@ class MovableObject extends DrawableObject {
     }
   
     this.lastHit = now;
-    this.energy -= 5;
+    this.energy -= 20;
   
     if (this.energy < 0) {
       this.energy = 0;
--- models/status-bar.class.js
+++ models/status-bar.class.js
@@ -1,13 +1,26 @@
 class StatusBar {
+
+    IMAGES_LIFE = [
+        'img/4. Marcadores/green/Life/0_  copia 3.png',
+        'img/4. Marcadores/green/Life/20_  copia 4.png',
+        'img/4. Marcadores/green/Life/40_  copia 3.png',
+        'img/4. Marcadores/green/Life/60_  copia 3.png',
+        'img/4. Marcadores/green/Life/80_  copia 3.png',
+        'img/4. Marcadores/green/Life/100_  copia 2.png'
+    ];
+
+    percentage = 100;
+
     constructor() {
-        this._status = 'ready';
+        this.loadImage(this.IMAGES_LIFE[5]);
+        this.loadImages(this.IMAGES_LIFE);
     }
 
-    get status() {
-        return this._status;
-    }
-
-    set status(value) {
-        this._status = value;
+    reducePercentage(amount) {
+        this.percentage -= amount;
+        if (this.percentage < 0) {
+            this.percentage = 0;
+        }
+        this.updateImage();
     }
 }
