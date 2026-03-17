f08bf530d6a142674ded519217f70107edc879b7
Janine Wächter <156570900+janinewchtr@users.noreply.github.com>
2026-03-17 16:34:40 +0100

    animation for final enemy / endboss

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -18,16 +18,51 @@ class Endboss extends MovableObject {
         "img/2.Enemy/3 Final Enemy/1.Introduce/10.png",
     ];
 
+    IMAGES_FLOATING = [
+
+        'img/2.Enemy/3 Final Enemy/2.floating/1.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/2.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/3.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/4.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/5.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/6.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/7.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/8.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/9.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/10.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/11.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/12.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/13.png',
+    ];
+
+    hadFirstContact = false; // Flag to track if the endboss has had first contact with the character
+
+
     constructor(){
-        super().loadImage(this.IMAGES_IDLE[0]);
+        super().loadImage('img/2.Enemy/3 Final Enemy/2.floating/1.png');
         this.loadImages(this.IMAGES_IDLE);
+        this.loadImages(this.IMAGES_FLOATING);
         this.x = 800; // Start position of the endboss
         this.animate(); // Start the animation of the endboss   
     }
 
     animate() {
-        setInterval(() => {
-            this.playAnimation(this.IMAGES_IDLE);
-          }, 200); // Adjust the interval for animation speed
+            let i = 0;
+        
+            setInterval(() => {
+                if (i < 10) {
+                    this.playAnimation(this.IMAGES_IDLE);
+                } else {
+                    this.playAnimation(this.IMAGES_FLOATING);
+                }
+        
+                i++;
+
+                if(World.character.x > 500 && !hadFirstContact) {
+                    i = 10;
+                    hadFirstContact = true;
+                }
+            }, 150);
         }
 }
+
