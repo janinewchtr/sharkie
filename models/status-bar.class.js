550cabfc5dd783a37815c3a6a1fe8955952428f6
Janine Wächter <156570900+janinewchtr@users.noreply.github.com>
2026-06-15 19:56:26 +0200

    bug fix

--- models/status-bar.class.js
+++ models/status-bar.class.js
@@ -5,67 +5,67 @@ class StatusBar extends DrawableObject {
   IMAGES_LIFE = IMAGE_PATHS.statusBar.life;
   IMAGES_COINS = IMAGE_PATHS.statusBar.coins;
   IMAGES_POISONED_BUBBLES = IMAGE_PATHS.statusBar.poison;
-  
-    percentage = 100;
-    images = [];
-  
-    /**
-     * Creates a status bar of the selected type.
-     * @param {string} type - Type of status bar: life, coin or poison.
-     */
-    constructor(type = "life") {
-      super();
-      if (type === "life") {
-        this.images = this.IMAGES_LIFE;
-      } else if (type === "coin") {
-        this.images = this.IMAGES_COINS;
-      } else if (type === "poison") {
-        this.images = this.IMAGES_POISONED_BUBBLES;
-      }
-  
-      this.loadImages(this.images);
-      this.x = 20;
-      this.y = 0;
-      this.width = 150;
-      this.height = 50;
-      this.setPercentage(100);
+
+  percentage = 100;
+  images = [];
+
+  /**
+   * Creates a status bar of the selected type.
+   * @param {string} type - Type of status bar: life, coin or poison.
+   */
+  constructor(type = "life") {
+    super();
+    if (type === "life") {
+      this.images = this.IMAGES_LIFE;
+    } else if (type === "coin") {
+      this.images = this.IMAGES_COINS;
+    } else if (type === "poison") {
+      this.images = this.IMAGES_POISONED_BUBBLES;
     }
-  
-    /**
-     * Sets the percentage and displays the matching status bar image.
-     * @param {number} percentage - Current percentage of the status bar.
-     */
-    setPercentage(percentage) {
-      this.percentage = percentage;
-      let path = this.images[this.getImageIndex()];
-      this.img = this.imageCache[path];
-    }
-  
-    /**
-     * Updates the status bar to the provided percentage.
-     * @param {number} percentage - New percentage of the status bar.
-     */
-    reducePercentage(percentage) {
-      this.setPercentage(percentage);
-    }
-  
-    /**
-     * Returns the image index matching the current percentage.
-     * @returns {number} Index of the status bar image.
-     */
-    getImageIndex() {
-      if (this.percentage >= 100) {
-        return 5;
-      } else if (this.percentage >= 80) {
-        return 4;
-      } else if (this.percentage >= 60) {
-        return 3;
-      } else if (this.percentage >= 40) {
-        return 2;
-      } else if (this.percentage >= 20) {
-        return 1;
-      } else {
-        return 0;
-      }
+
+    this.loadImages(this.images);
+    this.x = 20;
+    this.y = 0;
+    this.width = 150;
+    this.height = 50;
+    this.setPercentage(100);
+  }
+
+  /**
+   * Sets the percentage and displays the matching status bar image.
+   * @param {number} percentage - Current percentage of the status bar.
+   */
+  setPercentage(percentage) {
+    this.percentage = percentage;
+    let path = this.images[this.getImageIndex()];
+    this.img = this.imageCache[path];
+  }
+
+  /**
+   * Updates the status bar to the provided percentage.
+   * @param {number} percentage - New percentage of the status bar.
+   */
+  reducePercentage(percentage) {
+    this.setPercentage(percentage);
+  }
+
+  /**
+   * Returns the image index matching the current percentage.
+   * @returns {number} Index of the status bar image.
+   */
+  getImageIndex() {
+    if (this.percentage >= 100) {
+      return 5;
+    } else if (this.percentage >= 80) {
+      return 4;
+    } else if (this.percentage >= 60) {
+      return 3;
+    } else if (this.percentage >= 40) {
+      return 2;
+    } else if (this.percentage > 0) {
+      return 1;
+    } else {
+      return 0;
     }
   }
+}
